f5a172628777c168c7b7a2ea8ae9116825b13b5f
SuhasPalani <sugipals007@gmail.com>
2024-07-17 16:18:46 -0500

    ed of overview section

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import Products from "scenes/products";
 import Customers from "scenes/customers";
 import Transactions from "scenes/transactions";
 import Geography from "scenes/geography";
-// import Overview from "scenes/overview";
+import Overview from "scenes/overview";
 // import Daily from "scenes/daily";
 // import Monthly from "scenes/monthly";
 // import Breakdown from "scenes/breakdown";
@@ -33,12 +33,12 @@ function App() {
               <Route path="/customers" element={<Customers />} />
               <Route path="/transactions" element={<Transactions />} />
               <Route path="/geography" element={<Geography />} />
-              {/* <Route path="/overview" element={<Overview />} />
-              <Route path="/daily" element={<Daily />} />
+              <Route path="/overview" element={<Overview />} />
+              {/*<Route path="/daily" element={<Daily />} />
               <Route path="/monthly" element={<Monthly />} />
               <Route path="/breakdown" element={<Breakdown />} />
               <Route path="/admin" element={<Admin />} />
-              <Route path="/performance" element={<Performance />} /> */} 
+              <Route path="/performance" element={<Performance />} /> */}
             </Route>
           </Routes>
         </ThemeProvider>
