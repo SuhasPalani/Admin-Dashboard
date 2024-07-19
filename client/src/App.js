2ad902406d4aa3c0b550386ec9fc2bbaab171e1a
SuhasPalani <sugipals007@gmail.com>
2024-07-18 22:19:17 -0500

    end of breakdown page

--- client/src/App.js
+++ client/src/App.js
@@ -11,9 +11,9 @@ import Customers from "scenes/customers";
 import Transactions from "scenes/transactions";
 import Geography from "scenes/geography";
 import Overview from "scenes/overview";
-// import Daily from "scenes/daily";
-// import Monthly from "scenes/monthly";
-// import Breakdown from "scenes/breakdown";
+import Daily from "scenes/daily";
+import Monthly from "scenes/monthly";
+import Breakdown from "scenes/breakdown";
 // import Admin from "scenes/admin";
 // import Performance from "scenes/performance";
 
@@ -34,10 +34,10 @@ function App() {
               <Route path="/transactions" element={<Transactions />} />
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
-              {/*<Route path="/daily" element={<Daily />} />
+              <Route path="/daily" element={<Daily />} />
               <Route path="/monthly" element={<Monthly />} />
               <Route path="/breakdown" element={<Breakdown />} />
-              <Route path="/admin" element={<Admin />} />
+              {/*<Route path="/admin" element={<Admin />} />
               <Route path="/performance" element={<Performance />} /> */}
             </Route>
           </Routes>
